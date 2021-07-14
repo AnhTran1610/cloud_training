ec6bc14074d87979ac778f3509b3c362db5f9acc
hieuldt <46768555+hieuldt@users.noreply.github.com>
2021-07-14 15:32:43 +0700

    update

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "Amazon_Linux_2_AMI" {
   description = "Amazon Linux 2 AMI"
-  value       = aws_instance.web_server.ami
+  value       = aws_instance.webserver_instances[0].ami
 }
--- variables.tf
+++ variables.tf
@@ -7,3 +7,11 @@ variable "vpc_cidr" {
   type    = string
   default = "10.0.0.0/16"
 }
+
+variable "instance_count" {
+  default = "2"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}

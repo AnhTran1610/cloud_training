29bdb0ef8c32073c41e83b9bf9681995f8e8db58
AnhTran1610 <theanh161093@gmail.com>
2021-07-15 17:42:05 +0700

    .

--- aws_lb_target_group_attachment.tf
+++ aws_lb_target_group_attachment.tf
@@ -1,6 +1,6 @@
 resource "aws_lb_target_group_attachment" "web_service_group" {
-  count            = length(aws_instance.webserver_instances)
+  count            = length(aws_instance.webserver_ahta)
   target_group_arn = aws_lb_target_group.web_service_group.arn
-  target_id        = aws_instance.webserver_instances[count.index].id
+  target_id        = aws_instance.webserver_ahta[count.index].id
   port             = 80
 }

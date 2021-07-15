ed1bb2c7eed42cdabd88c5fe1751b719bc90c664
hieuldt <46768555+hieuldt@users.noreply.github.com>
2021-07-15 11:07:08 +0700

    bunch of updates

---
+++ aws_lb_target_group.tf
@@ -0,0 +1,15 @@
+resource "aws_lb_target_group" "web_service_group" {
+  name     = "web-service-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main_vpc.id
+  health_check {
+    port     = 80
+    protocol = "HTTP"
+    timeout  = 5
+    interval = 10
+  }
+  tags = {
+    Owner = "huyy"
+  }
+}
